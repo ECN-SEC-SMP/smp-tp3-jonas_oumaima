92dec9d0906e5186401fd8a4405002c06c100d87
skaiii <maouchejonas@gmail.com>
2025-11-15 18:40:05 +0100

    update "dilatation" function

--- filtragemorpho.cpp
+++ filtragemorpho.cpp
@@ -4,21 +4,17 @@
 #include "chargesauve.h"
 using namespace std;
 
-string filtre(t_Image *image,structelem *structure, int j, int i){
-    for (int k = 0; k < structure->h; k++){
-            for (int l = 0; l < structure->w; l++){
-                if ((image->im[j-(structure->y)+k+1][i-(structure->x)+l+1] > 0) && image->im[j][i] == 0){
-                    image->im[j][i] = 255;
-                }
-            }
-    }
-    return "Filtrée !";
-}
 
 string dilatation(structelem *structure, t_Image *image){
     for (int i = 0; i < image->h; i++ ){
         for (int k = 0; k< image->w; k++){
-                filtre(image, structure, i, k);
+            for (int m = 0; m < structure->h; m++){
+                for (int l = 0; l < structure->w; l++){
+                    if ((image->im[i-(structure->y)+m+1][k-(structure->x)+l+1] > 0) && image->im[i][k] == 0){
+                        image->im[i][k] = 255;
+                    }
+                }
+            }
         }
     }  
     return "Dilatée !";
