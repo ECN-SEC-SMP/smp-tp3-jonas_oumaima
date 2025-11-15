34b6277ca4a524ecefc0b04f5f09edb450d42c93
skaiii <maouchejonas@gmail.com>
2025-11-15 07:52:38 +0100

    Add .gitignore and improve image processing in main.cpp and outil.cpp

--- main.cpp
+++ main.cpp
@@ -8,18 +8,32 @@ using namespace std;
 int main(){
     int s;
     string Nomimage;
-    cout << "Nom de l'image :";
-    cin >> Nomimage;
-    t_Image image;
+    //cout << "Nom de l'image :";
+    //cin >> Nomimage;
+    t_Image *image = new t_Image;
+    t_Image *image1 = new t_Image;
+    t_Image *image2 = new t_Image;
     bool Ok;
-    loadPgm(Nomimage, &image, Ok);
+    loadPgm("paysage.pgm", image, Ok);
 
     cout << "Taille de l'image : ";
-    cout << image.w << "x" << image.h << endl;
+    cout << image->w << "x" << image->h << endl;
 
     cout << "Valeur de seuillage : " ;
     cin >> s;
-    seuillage(&image,s);
-    savePgm(Nomimage + "_seuillé",  &image);
+    seuillage(image,s);
+    savePgm(Nomimage + "_seuillée   ",  image);
 
+    cout << "Dfférence entre monarch512x512 et mri512x512" << endl;
+    loadPgm("monarch512x512.pgm", image1, Ok);
+    loadPgm("mri512x512.pgm", image2, Ok);
+    cout << "Taille moarch512x512.pgm : "<< image1->w << "x" << image1->h << endl; 
+    cout << "image1 -> h = " << image1->h;
+    cout << "Taille mri512x512.pgm : "<< image2->w << "x" << image2->h << endl; 
+
+    diff(image1, image2);
+
+    delete image;
+    delete image1;
+    delete image2;
 }
--- outil.cpp
+++ outil.cpp
@@ -6,7 +6,7 @@
 #include <assert.h>
 using namespace std;
 
-void seuillage(t_Image *image, int s){
+void seuillage(t_Image * image, int s){
     for (int i = 0; i <= image->h; i++ ){
         for (int k = 0; k<= image ->w; k++){
             if (image->im[i][k] < s){
@@ -20,15 +20,18 @@ void seuillage(t_Image *image, int s){
 }
 
 int diff(t_Image *image1, t_Image *image2){
-    assert(image1->h == image2->h && image2->w == image2->w);
+   // assert(not(image1->h == image2->h && image1->w == image2->w));
+   cout<<image1->h;
     int d = 0;
-    for (int i = 0; i <= image1->h; i++ ){
-        for (int j = 0; j<= image1->w; j++){
+    for (int i = 0; i < image1->h; i++ ){
+        for (int j = 0; j< image1->w; j++){
         d = image1->im[i][j] - image2->im[i][j];
         if (d < 0){
-            d = d*(-1);
+            d = -d;
         }
-        return d;
+        //cout << "i = " << i << endl;
+        //cout << "j = " << j << endl;
+        //cout << "("<<i<< "," << j<<")" << ":" << d << endl;
         }
     }
 }
