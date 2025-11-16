3ca3ea2383efc5b99cc00fbe26dacdf0a7f821a7
skaiii <maouchejonas@gmail.com>
2025-11-16 14:51:58 +0100

    add comments for "outil.cpp" and "outil.h"

--- outil.cpp
+++ outil.cpp
@@ -6,27 +6,28 @@
 #include <assert.h>
 using namespace std;
 
+
 void seuillage(t_Image * image, unsigned int s){
-    for (int i = 0; i < image->h; i++ ){
+    for (int i = 0; i < image->h; i++ ){ // On parcourt l'ensemble des pixels de l'image
         for (int k = 0; k< image ->w; k++){
-            if (image->im[i][k] < s){
-                image->im[i][k] = 0;
+            if (image->im[i][k] < s){ // On verifie si le niveau de gris est inferieur à la valeur de seuillage
+                image->im[i][k] = 0; // Si oui : le niveau de gris passe à 0
             }
             else {
-                image->im[i][k] = 255;
+                image->im[i][k] = 255; // Sinon : le niveau de gris passe à 255
             }
         }
     }
 }
 
 int diff(t_Image *image1, t_Image *image2, int i, int j){
-    assert(image1->h == image2->h && image1->w == image2->w);
-    int d = 0;
-    d = image1->im[i][j] - image2->im[i][j];
+    assert(image1->h == image2->h && image1->w == image2->w); // On vérifie bien que les deux images sont de même taille
+    int d = 0; // On initialise la valeur de la difference 
+    d = image1->im[i][j] - image2->im[i][j]; // on calcule la différence des niveau de gris 
     if (d < 0){
-        return -d;
+        return -d; // On renvoie l'opposé de la valeur si d < 0, pour que ce soit positif
     }
     else {
-        return d;
+        return d; // On renvoie la valeur si d > 0
     }
 }
--- outil.h
+++ outil.h
@@ -1,8 +1,12 @@
 #include "image.h"
-// Fonction seuillage à un niveau s d'une image
-// Remplace les pixels d'un niveau de gris inférieur à s par 0 
-// Remplace les pixels d'un niveau de gris supérieur à s par 255
+/*
+Fonction seuillage à un niveau s d'une image
+Remplace les pixels d'un niveau de gris inférieur à s par 0 
+Remplace les pixels d'un niveau de gris supérieur à s par 255 */
 void seuillage(t_Image *image, unsigned int s);
-// Fonction différence entre deux images de même taille 
-// Renvoie la valeur absolue des valeurs de chaque pixels 
+
+
+/*
+Fonction différence entre deux images de même taille 
+Renvoie la valeur absolue de la difference de niveau de gris des pixels des deux images*/
 int diff(t_Image *image1, t_Image *image2, int i, int j);
